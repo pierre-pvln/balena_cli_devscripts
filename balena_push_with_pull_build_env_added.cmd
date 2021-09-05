fd1931742ada3833e25830ffb5b46d556ce076f4
IPHPY38PIP <info@ipheion.eu>
2021-09-05 08:41:59 +0200

    added scripts for setting environment vars

--- balena_push_with_pull_build_env_added.cmd
+++ balena_push_with_pull_build_env_added.cmd
@@ -20,12 +20,13 @@ SET PDRIVE=%~d0
 :: Setting the directory and drive of this commandfile
 SET CMD_DIR=%~dp0
 
+CD %CMD_DIR%
 CALL .\utils\balena_organization.cmd
-CD %CMD_DIR%
 
+CD %CMD_DIR%
 CALL .\utils\balena_fleet.cmd
-CD %CMD_DIR%
 
+CD %CMD_DIR%
 ECHO [INFO ] Start building container(s) ...
 ECHO [INFO ] Building as:
 CALL "C:\Program Files\balena-cli\bin\balena" whoami
@@ -40,15 +41,11 @@ CD ..\containers
 ::call "C:\Program Files\balena-cli\bin\balena" push daya-mqtt-python-64 --nocache --debug
 CALL "C:\Program Files\balena-cli\bin\balena" push %BALENA_ORGANIZATION%/%BALENA_FLEET% --pull
 
-:: https://www.balena.io/docs/reference/balena-cli/#envs
-:: set environment variables for usage with AIS dAISy HAT rpi
-:: Disable bluetooth serial
-CALL "C:\Program Files\balena-cli\bin\balena" env add BALENA_HOST_CONFIG_dtoverlay "\"vc4-fkms-v3d\", \"disable-bt\"" --application %BALENA_ORGANIZATION%/%BALENA_FLEET%
-CALL "C:\Program Files\balena-cli\bin\balena" env add RESIN_HOST_CONFIG_dtoverlay "\"vc4-fkms-v3d\", \"disable-bt\"" --application %BALENA_ORGANIZATION%/%BALENA_FLEET%
-:: Enable uart 
-CALL "C:\Program Files\balena-cli\bin\balena" env add BALENA_HOST_CONFIG_enable_uart "1" --fleet %BALENA_ORGANIZATION%/%BALENA_FLEET%
-CALL "C:\Program Files\balena-cli\bin\balena" env add RESIN_HOST_CONFIG_enable_uart "1" --fleet %BALENA_ORGANIZATION%/%BALENA_FLEET%
+CD %CMD_DIR%
+CALL .\envs\cm4io_usb_on.cmd
+
+::CD %CMD_DIR%
+::CALL .\envs\ais_daysi_hat.cmd
 
 CD %CMD_DIR%
-
 PAUSE
---
+++ envs/cm4io_usb_on.cmd
@@ -0,0 +1,8 @@
+ECHO [INFO ] Setting environment variables for enabling USB on CM4 IO board ...
+:: https://www.balena.io/docs/reference/balena-cli/#envs
+:: https://forums.balena.io/t/cm4-sierra-em7345-modem/328468/5
+:: https://forums.balena.io/t/balena-cm4-io-board-image-usb-support/338550/3
+:: set environment variables for usage with cm4 io board
+:: enable USB
+CALL "C:\Program Files\balena-cli\bin\balena" env add BALENA_HOST_CONFIG_dtoverlay "\"vc4-fkms-v3d\", \"dwc2,dr_mode=host\"" --fleet %BALENA_ORGANIZATION%/%BALENA_FLEET%
+CALL "C:\Program Files\balena-cli\bin\balena" env add RESIN_HOST_CONFIG_dtoverlay "\"vc4-fkms-v3d\", \"dwc2,dr_mode=host\"" --fleet %BALENA_ORGANIZATION%/%BALENA_FLEET%
