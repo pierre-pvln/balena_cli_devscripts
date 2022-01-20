50823587215d3c51dcec3f9a028420dc845f49c8
IPHPY38PIP <info@ipheion.eu>
2022-01-20 20:30:17 +0100

    Cretaing treefile command added

---
+++ create_containers_version_tree_file.cmd
@@ -0,0 +1,76 @@
+@ECHO off
+::
+:: @name:     create_containers_tree_file.cmd
+:: @purpose:  create file with the tree of all the files for a balena fleet version or release
+::
+:: @version   v0.0.1  2022-01-20
+:: @author    pierre@ipheion.eu
+:: @copyright (C) 2020-2021 Pierre Veelen
+::
+
+SETLOCAL ENABLEEXTENSIONS
+
+:: BASIC SETTINGS
+:: ==============
+:: Setting the name of the script
+SET ME=%~n0
+:: Setting the name of the directory
+SET PARENT=%~p0
+SET PDRIVE=%~d0
+:: Setting the directory and drive of this commandfile
+SET CMD_DIR=%~dp0
+
+SET ERROR_MESSAGE=[INFO ] No error ...
+
+:: BALENA SETTINGS
+:: ===============
+SET "BALENA_CLI=C:\Program Files\balena-cli\bin\balena"
+
+ECHO [INFO ] Start running %ME%
+ECHO.
+:: Check balenadev scripts with github 
+:: ===================================
+ECHO [INFO ] Are we up to date with the balenadev scripts? ...
+::    -s, --short           show status concisely
+::    -b, --branch          show branch information
+git status -s -b
+ECHO.
+timeout /T 5
+CD %CMD_DIR%
+
+CALL .\utils\balena_login.cmd
+IF %ERRORLEVEL% NEQ 0 (
+	SET ERROR_MESSAGE=[ERROR] Setting login credentials not succesfull ...  		
+	GOTO ERROR_EXIT
+)
+CD %CMD_DIR%
+
+CALL .\utils\balena_organization.cmd
+CD %CMD_DIR%
+
+CALL .\utils\balena_fleet.cmd
+CD %CMD_DIR%
+
+CALL .\utils\balena_version.cmd
+CD %CMD_DIR%
+
+::	 
+:: The actual programs/scripts to run
+::
+ECHO.
+ECHO [INFO ] Creating file ..\containers\fleet_containers_tree.txt ...
+tree /f /a ..\containers >..\containers\fleet_containers_tree.txt
+CD %CMD_DIR%
+
+ECHO.
+GOTO CLEAN_EXIT
+
+:ERROR_EXIT
+ECHO %ERROR_MESSAGE%
+ECHO.
+
+:CLEAN_EXIT
+CD %CMD_DIR%
+
+ECHO.
+PAUSE
